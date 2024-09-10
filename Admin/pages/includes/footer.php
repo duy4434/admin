73affa9167932c71bd4913a6da9afb81a17fabc9
duy4434 <46771356+duy4434@users.noreply.github.com>
2024-09-11 01:17:11 +0700

    text

---
+++ Admin/pages/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.0.0-alpha
+    </div>
+    <strong>Copyright &copy; 2025-2026 <a href="">Work_shop krooteerapong </a></strong>AdminLTE All rights
+    reserved.
+</footer>
